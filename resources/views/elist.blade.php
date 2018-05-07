4cd39ef145356345327a68a314a91d25e603f6f0
alam3211 <alamcahya3211@gmail.com>
2018-05-07 22:04:25 +0700

    EventList

--- resources/views/elist.blade.php
+++ resources/views/elist.blade.php
@@ -1,21 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container" style="background-color: white;">
-		h
+	<div class="container rounded" style="background-color: white; padding: 10px;">
+		<div class="container rounded" style="background-color: grey;margin: 5px;"">
 		<div class="row">
-			<div class="container" style="background-color: blue;">
-				h
+				<div class="col-md-2">
+					<img class="img-thumbnail float-left" src="images/thm.jpg">
+				</div>
+				<div class="col-md-6">
+					<h3>CAMP FAIR 2012</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+				<div class="col-md-4">
+					<table>
+						<thead>
+							<tr>
+								<th>Jadwal</th>
+								<th>Harga</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Sabtu, 5 Maret 2019 11.00 PM - 12.00 PM Gedung Aiola, Surabaya</td>
+								<td>150K - 300K</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
+		<div class="container rounded" style="background-color: grey;margin: 5px;"">
 		<div class="row">
-			<div class="container" style="background-color: blue;">
-			h
-			</div>
-		</div>
-		<div class="row">
-			<div class="container" style="background-color: blue;">
-			h
+				<div class="col-md-2">
+					<img class="img-thumbnail float-left" src="images/thm.jpg">
+				</div>
+				<div class="col-md-6">
+					<h3>CAMP FAIR 2012</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+				<div class="col-md-4">
+					<table>
+						<thead>
+							<tr>
+								<th>Jadwal</th>
+								<th>Harga</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Sabtu, 5 Maret 2019 11.00 PM - 12.00 PM Gedung Aiola, Surabaya</td>
+								<td>150K - 300K</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
