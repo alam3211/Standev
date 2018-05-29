28e8c3f46338aabda9ed1ebc720bba09336cc801
Aldinata Rizky Revanda <5116100023@mahasiswa.integra.its.ac.id>
2018-05-29 15:48:30 +0700

    tambah konten
    sak enek e

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -9,7 +9,9 @@
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<p id="aboutus">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p id="aboutus">StandEv is a website platform which is used to bring together the event organizers with tenants.
+				<br>StandEv is an innovation that will provide benefits for both parties, the event organizers will no longer difficulties in finding the seller for the event stand and the tenants will be facilitated in finding the nearest event for them to sell.
+				<br>StandEv was founded in May 2018 and made by PotatoDev team.</p>
 			</div>
 		</div>
 	</div>
